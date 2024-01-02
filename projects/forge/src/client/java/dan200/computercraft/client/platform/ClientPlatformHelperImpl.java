4f0bf2ef83ecbdc9943488d39b129695ef3ce49f
Jonathan Coates <git@squiddev.cc>
2024-01-02 13:25:36 +0000

    Add some debug logging for missing model errors

--- projects/forge/src/client/java/dan200/computercraft/client/platform/ClientPlatformHelperImpl.java
+++ projects/forge/src/client/java/dan200/computercraft/client/platform/ClientPlatformHelperImpl.java
@@ -6,6 +6,8 @@ package dan200.computercraft.client.platform;
 
 import com.google.auto.service.AutoService;
 import com.mojang.blaze3d.vertex.PoseStack;
+import cpw.mods.modlauncher.Launcher;
+import cpw.mods.modlauncher.api.IEnvironment;
 import dan200.computercraft.client.model.FoiledModel;
 import dan200.computercraft.client.render.ModelRenderer;
 import dan200.computercraft.shared.network.NetworkMessage;
@@ -19,18 +21,55 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.model.data.ModelData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @AutoService(dan200.computercraft.impl.client.ClientPlatformHelper.class)
 public class ClientPlatformHelperImpl implements ClientPlatformHelper {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientPlatformHelperImpl.class);
+
     private static final RandomSource random = RandomSource.create(0);
     private static final Direction[] directions = Arrays.copyOf(Direction.values(), 7);
 
+    private boolean reportedMissing = false;
+
     @Override
     public BakedModel getModel(ModelManager manager, ResourceLocation location) {
-        return manager.getModel(location);
+        var model = manager.getModel(location);
+        if (model == null) {
+            // This should never happen, but it does anyway! See https://github.com/cc-tweaked/CC-Tweaked/issues/1626
+            reportMissingModel(manager, location);
+            model = manager.getMissingModel();
+        }
+
+        return model;
+    }
+
+    private void reportMissingModel(ModelManager manager, ResourceLocation location) {
+        if (reportedMissing) return;
+        reportedMissing = true;
+
+        LOG.error(
+            """
+                ModelManager.getModel("{}") returned null. This should never happen, and indicates the model registry is corrupt.
+
+                ModelManager has been transformed by the following transformers: {}.
+                The following models are null: {}.""",
+            location,
+            Optional.ofNullable(Launcher.INSTANCE)
+                .flatMap(env -> env.environment().getProperty(IEnvironment.Keys.AUDITTRAIL.get()))
+                .map(x -> x.getAuditString(ModelManager.class.getName()))
+                .orElse("unknown"),
+            manager.bakedRegistry.entrySet().stream()
+                .filter(x -> x.getValue() == null)
+                .map(x -> x.getKey().toString())
+                .collect(Collectors.joining(", "))
+        );
     }
 
     @Override
